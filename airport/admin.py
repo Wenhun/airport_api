01347d1ba790b3eace8f9480f9fb152abfc06890
Ilia Terentev <wenhun1@gmail.com>
2025-04-25 17:21:18 +0300

    Register models in admin for airport and user apps; update requirements for pillow and pycodestyle

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+import airport.models as models
+from django.contrib.auth.models import Group
+
+admin.site.register(models.Coutry)
+admin.site.register(models.City)
+admin.site.register(models.Airport)
+admin.site.register(models.Route)
+admin.site.register(models.AirplaneType)
+admin.site.register(models.Airplane)
+admin.site.register(models.Flight)
+admin.site.register(models.Order)
+admin.site.register(models.Ticket)
+admin.site.register(models.Position)
+admin.site.register(models.Crew)
+admin.site.unregister(Group)

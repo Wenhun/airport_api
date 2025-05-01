f6a1b2c2fb9297af9d736022938e94ab99b07ed3
Ilia Terentev <wenhun1@gmail.com>
2025-05-01 16:47:16 +0300

    Refactor UserSerializer to specify return types for create and update methods; enhance type clarity with AbstractUser

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 
+from django.contrib.auth.models import AbstractUser
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -8,11 +10,11 @@ class UserSerializer(ModelSerializer):
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
 
-    def create(self, validated_data: dict) -> get_user_model():
+    def create(self, validated_data: dict) -> AbstractUser:
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
-    def update(self, instance: get_user_model(), validated_data: dict) -> get_user_model():
+    def update(self, instance: AbstractUser, validated_data: dict) -> AbstractUser:
         """Update a user, set the password correctly and return it"""
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
